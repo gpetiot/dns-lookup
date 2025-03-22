a5bb0f5ec02b475a45ea448cd04aeec3b4f44762
Guillaume Petiot <hello@guillaumepetiot.com>
2025-03-22 19:06:56 +0000

    Refactor(icons): Extract all SVG icons into separate reusable components

--- src/components/DomainResult.js
+++ src/components/DomainResult.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { getPricing, getRenewalPrice } from '../services/porkbunService';
 import ExternalLinkIcon from './icons/ExternalLinkIcon';
+import LoadingIcon from './icons/LoadingIcon';
+import ErrorIcon from './icons/ErrorIcon';
+import AvailableIcon from './icons/AvailableIcon';
+import RegisteredIcon from './icons/RegisteredIcon';
+import RetryIcon from './icons/RetryIcon';
 
 const DomainResult = ({ domain, data, loading, onRetry }) => {
   const [price, setPrice] = useState(null);
@@ -34,22 +39,13 @@ const DomainResult = ({ domain, data, loading, onRetry }) => {
       <div className="flex items-center flex-grow">
         <div className="flex-shrink-0 w-8 mr-2">
           {loading ? (
-            <svg className="animate-spin h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
+            <LoadingIcon />
           ) : hasError ? (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg>
+            <ErrorIcon />
           ) : isAvailable ? (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-            </svg>
+            <AvailableIcon />
           ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-            </svg>
+            <RegisteredIcon />
           )}
         </div>
         
@@ -92,14 +88,7 @@ const DomainResult = ({ domain, data, loading, onRetry }) => {
                 onClick={onRetry}
                 className="px-2 py-1 text-xs bg-blue-500 hover:bg-blue-600 text-white font-medium rounded transition duration-200"
               >
-                <svg 
-                  xmlns="http://www.w3.org/2000/svg" 
-                  className="h-3 w-3 inline-block mr-1" 
-                  viewBox="0 0 20 20" 
-                  fill="currentColor"
-                >
-                  <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
-                </svg>
+                <RetryIcon />
                 Retry
               </button>
             )}
