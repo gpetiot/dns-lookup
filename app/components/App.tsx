b5e7542bde6259ec31f7851771fbf408af7e0c67
Guillaume Petiot <hello@guillaumepetiot.com>
2025-04-27 21:56:58 +0100

    Refactor(icons): extract search icon into separate component

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -7,6 +7,7 @@ import NoResultPlaceholder from './NoResultPlaceholder';
 import LoadingIcon from './icons/LoadingIcon';
 import CopyIcon from './icons/CopyIcon';
 import CheckIcon from './icons/CheckIcon';
+import SearchIcon from './icons/SearchIcon';
 import { useDomainState } from '@/hooks/useDomainState';
 import { useAISuggestions } from '@/hooks/useAISuggestions';
 import { useFilters } from '@/hooks/useFilters';
@@ -98,18 +99,7 @@ function App() {
           <form onSubmit={handleSubmit} className="w-full">
             <div className="relative flex items-center">
               <span className="pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <SearchIcon />
               </span>
               <input
                 type="text"
